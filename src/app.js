09424c5121fe0bf1d7a1121f1d0e0ce7980c166b
hamzahasn <hamza.hasnain@gmail.com>
2021-05-27 00:38:55 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,27 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
+window.onload = () => {
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#restaurant").innerHTML = generateRestaurant();
+  });
   console.log("Hello Rigo from the console!");
+};
+
+let generateRestaurant = () => {
+  let restaurant = [
+    "Chipotle",
+    "Salsa Fiesta",
+    "Chick-Fil-A",
+    "Panera Bread",
+    "Burger-Fi",
+    "New York Grilled Cheese",
+    "Olive Garden",
+    "Chicken Guy",
+    "Tropical Smoothie Cafe"
+  ];
+
+  let rIndex = Math.floor(Math.random() * restaurant.length);
+
+  return restaurant[rIndex];
 };
